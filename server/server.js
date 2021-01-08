2a155242f2af2286aaec2693720473c43b16cf76
karan1206 <64606159+karanjoshi1206@users.noreply.github.com>
2021-01-08 17:02:21 +0530

    add custom events

--- server/server.js
+++ server/server.js
@@ -21,6 +21,50 @@ app.use(express.static(publicPath))
 io.on("connection", (socket) => {
     console.log("someone new is connected")
 
+    // to listen a new message from client side we use emit here
+
+    //initial message to everone
+    socket.emit("newMessage", {
+        from: "admin",
+        text: "Welcome to the chat app !!",
+        createdAt: new Date().getTime()
+    })
+
+    // when someone join everone except the joining person will get the message
+    socket.broadcast.emit("newMessage", {
+        from: "admin",
+        text: "Someone new is join",
+        createdAt: new Date().getTime()
+    })
+
+
+
+
+    // now we will create our custom event other than connection and disconnection
+
+    socket.on("createMessage", (message) => {
+        console.log("createMessage", message)
+
+        // io.emit will brodcast this to each channel or member connected to the server (socket.emit will send only to that member from which the request is comming)
+        io.emit("newMessage", {
+            from: message.from,
+            message: message.text,
+            createdAt: new Date().getTime()
+        })
+
+
+        // this will broadcast the message to everyobe except the user who created the event 
+        // socket.broadcast.emit("newMessage", {
+        //     from: message.from,
+        //     message: message.text,
+        //     createdAt: new Date().getTime()
+        // })
+    })
+
+
+
+
+
     // we use the socket object to check if someone disonnected 
     socket.on("disconnect", () => {
         console.log("disconnected")
